5332e75ff05ab7953e483bc6d3c12c492d1464bc
Olli Meier <hallo@olli-meier.de>
2017-05-03 23:10:48 +0200

    New script to check weird kerning
    Very simple script.

---
+++ omWeirdKerningCheck.py
@@ -0,0 +1,52 @@
+#MenuTitle: check weird kerning
+# -*- coding: utf-8 -*-
+# Code by Olli Meier, Mai 2016
+__doc__="""
+Check weird Kerning for selected master.
+"""
+
+from GlyphsApp import *
+
+font = Glyphs.font
+
+def weirdKerningCheck(font=None):
+	kernGroup = 0
+	totalKern = 0
+	kernValues = []
+	if font:
+		for master in font.kerning:
+			kernGroup += len(font.kerning[master])
+			masterKern = font.kerning[master]
+			for anyKernGroup in masterKern:
+				totalKern += len(masterKern[anyKernGroup])
+				singleKern = masterKern[anyKernGroup]
+				for kernValue in singleKern:
+					kernValues.append(singleKern[kernValue])
+
+	
+	return kernGroup, everyKern, kernValues
+
+def getGlyphWidth(font=None):
+	glyphWidth = {}
+	if font:
+		for anyG in font.glyphs:
+			#glyphWidth.append(anyG.layers[font.selectedFontMaster.id].width)
+			glyphWidth[anyG.name] = anyG.layers[font.selectedFontMaster.id].width
+
+	return glyphWidth
+	
+
+if __name__ == "__main__":
+	
+	kernGroup, everyKern, kernValues = weirdKerningCheck(font)
+	glyphWidth = getGlyphWidth(font)
+	content = 'Kerning groups: ' + str(kernGroup) + '\n' + 'Number of kerning pairs: ' + str(everyKern) + '\n' + 'Min kerning: ' + str(min(kernValues))
+	
+	generalGlyphwidth = []
+	for anyG in glyphWidth:
+		generalGlyphwidth.append(glyphWidth[anyG])
+	
+	if (min(kernValues) * -1) >= (max(generalGlyphwidth) / 2):
+		content += '\nWarning: There is a kerning larger than half of the biggest glyph width.'
+	
+	Glyphs.displayDialog_(str(content))
