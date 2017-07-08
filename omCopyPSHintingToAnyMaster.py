786563d85b2655aa1480bd4ce3edc21971b37488
Olli Meier <hallo@olli-meier.de>
2017-07-08 11:22:29 +0200

    new script, copy hinting from one layer to another
    Script for GlpyhsApp.

---
+++ omCopyPSHintingToAnyMaster.py
@@ -0,0 +1,25 @@
+#MenuTitle: copy hinting from one layer to another
+# -*- coding: utf-8 -*-
+# Code by Olli Meier, July 2017
+# Version 1.0
+__doc__="""
+Copy PS and TT hinting for each glyph from one layer to another.
+"""
+
+from GlyphsApp import *
+
+font = Glyphs.font
+import copy
+
+
+for g in font.glyphs:
+	hintedLayer = ''
+	for layer in g.layers:
+		if layer.hints:
+			hintedLayer = layer
+			break
+	if hintedLayer:
+		for layer in g.layers:
+			if layer != hintedLayer and not layer.hints:
+				layer.hints = copy.copy(hintedLayer.hints)
+				print 'Glyph: %s, copied hinting from %s to %s.' %(g.name, hintedLayer, layer)
