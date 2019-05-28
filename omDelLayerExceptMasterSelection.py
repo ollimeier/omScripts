3d5b38c468f35811334d1566fc15f5d25557e81e
Olli Meier <hallo@olli-meier.de>
2019-05-28 08:56:06 +0200

    Update omDelLayerExceptMasterSelection.py
    Update script to delete only layers which are not master layers or other  special layers

--- omDelLayerExceptMasterSelection.py
+++ omDelLayerExceptMasterSelection.py
@@ -10,15 +10,27 @@ Deletes all layers except masters for all selected glyphs.
 from GlyphsApp import *
 f = Glyphs.font
 
-masterName = []
-for master in f.masters:
-	masterName.append(master.name)
+# masterName = []
+# for master in f.masters:
+# 	masterName.append(master.name)
 
 def delLayers(g):
 	delList = []
 	for x, layer in enumerate(g.layers):
-		if layer.name not in masterName:
+		#if layer.name not in masterName:
+
+		if not (layer.isMasterLayer() or layer.isBraceLayer() or layer.isSpecialLayer):
 			delList.append(x)
+		'''
+		 	print ('layer Master: ', layer.name)
+		elif layer.isBraceLayer():
+		 	print ('layer Brace: ', layer.name)
+		elif layer.isSpecialLayer:
+			print ('layer Special: ', layer.name)
+		else:
+			print ('layer.name to delete: ', layer.name)
+			#delList.append(x)
+		'''
 
 	for l in reversed(delList): #need to start with the highest value
 		print ('del layer: %s' %g.layers[l])
